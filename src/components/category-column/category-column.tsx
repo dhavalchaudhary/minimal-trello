23cbe09a7bec9cece64bb458916bfe54c4e61df4
Dhaval Chaudhary <dhavalchaudhary96@gmail.com>
2022-03-09 17:52:46 +0530

    add card display ui

--- src/components/category-column/category-column.tsx
+++ src/components/category-column/category-column.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { mockCardsData } from '../../mockData/cards';
 import { Category } from '../../types';
 
 type CategoryColumnProps = {
@@ -10,6 +11,17 @@ export const CategoryColumn: React.FC<CategoryColumnProps> = ({data}) => {
     <div className="category-title-wrapper">
       <h3>{data.title}</h3>
     </div>
-    <div className="card-list-wrapper"></div>
+    <div className="card-list-wrapper">
+        {mockCardsData.map(card => (
+            <div className="card">
+                <h5>{card.title}</h5>
+                <div className="button-group card-button-wrapper">
+                    <button>Move Left</button>
+                    <button>Edit</button>
+                    <button>Move Right</button>
+                </div>
+          </div>
+        ))}
+    </div>
   </div>
 }
---
+++ src/mockData/cards.ts
@@ -0,0 +1,13 @@
+import { Card } from "../types";
+import { v4 as uuidv4 } from 'uuid';
+
+export const mockCardsData: Card[] = [
+    {
+        title: 'Task 1',
+        id: uuidv4()
+    },
+    {
+        title: 'Task 2',
+        id: uuidv4()
+    }
+]

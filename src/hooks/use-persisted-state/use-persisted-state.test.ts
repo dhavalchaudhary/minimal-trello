ab66df892689dfee1cb27f4284aaa07e50706560
Dhaval Chaudhary <dhavalchaudhary96@gmail.com>
2022-03-09 20:14:03 +0530

    test hooks

--- src/hooks/use-persisted-state/use-persisted-state.test.ts
+++ src/hooks/use-persisted-state/use-persisted-state.test.ts
@@ -1,7 +1,34 @@
 import React from 'react';
+import { renderHook, cleanup, act } from '@testing-library/react-hooks'
+import {usePersistedState} from './use-persisted-state';
+
+const mockKey = 'test-key';
+const mockValue = {data: 'mock-value'};
+const mockDefaultValue = {data: 'mock-default-value'};
 
 describe('usePersistedState', () => {
-    it.todo('returns default value if no value is persisted')
-    it.todo('returns the persisted value if valid')
-    it.todo('persists the value in local storage when the value is updated')
+    afterEach(() => {
+        window.localStorage.removeItem(mockKey);
+        cleanup()
+    })
+
+    it('returns default value if no value is persisted', () => {
+        const {result} = renderHook(() => usePersistedState(mockKey, mockDefaultValue));
+        expect(result.current[0]).toStrictEqual(mockDefaultValue)
+    })
+
+    it('returns the persisted value if valid', () => {
+        window.localStorage.setItem(mockKey, JSON.stringify(mockValue));
+        const {result} = renderHook(() => usePersistedState(mockKey, mockDefaultValue));
+        expect(result.current[0]).toStrictEqual(mockValue)
+    })
+
+    it('persists the value in local storage when the value is updated', () => {
+        const {result} = renderHook(() => usePersistedState(mockKey, mockDefaultValue));
+        
+        act(() => {result.current[1](mockValue)});
+
+        expect(result.current[0]).toStrictEqual(mockValue);
+        expect(JSON.parse(window.localStorage.getItem(mockKey) as string)).toStrictEqual(mockValue)
+    })
 })

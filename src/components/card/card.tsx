12dd97df8961c8293e0aca0aab0cccaad00aabdd
Dhaval Chaudhary <dhavalchaudhary96@gmail.com>
2022-03-09 18:12:44 +0530

    add card edit functionality

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { Card as CardType } from '../../types';
 
 type CardProps = {
@@ -6,12 +6,39 @@ type CardProps = {
 }
 
 export const Card:React.FC<CardProps> = ({data}) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [titleInputVal, setTitleInputVal] = useState(data.title);
+    
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitleInputVal(e.target.value)
+    }
+
+    const resetState = () => {
+        setIsEditing(false)
+    }
+
+    const saveData = () => {
+        // sync with state
+        console.log(titleInputVal);
+        resetState()
+    }
+
     return <div className="card">
+    {isEditing ? <>
+        <input className="card-edit-input" placeholder="add card title" value={titleInputVal} onChange={handleInputChange} />
+          <div className="button-group card-button-wrapper">
+            <button onClick={resetState}>Cancel</button>
+            <button onClick={saveData}>Save</button>
+          </div>
+    </> : <>
     <h5>{data.title}</h5>
     <div className="button-group card-button-wrapper">
       <button>Move Left</button>
-      <button>Edit</button>
+      <button onClick={() => setIsEditing(true)}>Edit</button>
       <button>Move Right</button>
     </div>
+    </>}
+    
+    
   </div>
 }
